c7d45749352e43da2bd0801e4206b2e02da82bcb
Wolf McNally <wolf@wolfmcnally.com>
2023-05-14 01:10:40 -0700

    Working.

--- src/assertions.rs
+++ src/assertions.rs
@@ -1,10 +1,9 @@
-use crate::Envelope;
+// use crate::Envelope;
 
-// /// Support for manipulating assertions.
+// Support for manipulating assertions.
 
 // impl Envelope {
 //     pub fn add_assertion(assertion: Option<&Envelope>, salted: bool) -> AsRef<Envelope> {
-//         match
 //         todo!();
 //     }
 // }
