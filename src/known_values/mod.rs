ecf61a9b31c164be6844bf4ca172825898d9fef2
Wolf McNally <wolf@wolfmcnally.com>
2023-09-25 01:21:19 -0700

    Cleanup.

--- src/known_values/mod.rs
+++ src/known_values/mod.rs
@@ -19,56 +19,6 @@ macro_rules! known_value_constant {
     };
 }
 
-// ```swift
-// public extension KnownValue {
-//     static let isA = KnownValue(1, "isA")
-//     static let id = KnownValue(2, "id")
-//     static let verifiedBy = KnownValue(3, "verifiedBy")
-//     static let note = KnownValue(4, "note")
-//     static let hasRecipient = KnownValue(5, "hasRecipient")
-//     static let sskrShare = KnownValue(6, "sskrShare")
-//     static let controller = KnownValue(7, "controller")
-//     static let publicKeys = KnownValue(8, "publicKeys")
-//     static let dereferenceVia = KnownValue(9, "dereferenceVia")
-//     static let entity = KnownValue(10, "entity")
-//     static let hasName = KnownValue(11, "hasName")
-//     static let language = KnownValue(12, "language")
-//     static let issuer = KnownValue(13, "issuer")
-//     static let holder = KnownValue(14, "holder")
-//     static let salt = KnownValue(15, "salt")
-//     static let date = KnownValue(16, "date")
-//     static let unknown = KnownValue(17, "Unknown")
-//     static let diffEdits = KnownValue(20, "edits")
-//     static let attachment = KnownValue(50, "attachment")
-//     static let vendor = KnownValue(51, "vendor")
-//     static let conformsTo = KnownValue(52, "conformsTo")
-//     static let body = KnownValue(100, "body")
-//     static let result = KnownValue(101, "result")
-//     static let error = KnownValue(102, "error")
-//     static let OK = KnownValue(103, "OK")
-//     static let Processing = KnownValue(104, "Processing")
-//     static let Seed = KnownValue(200, "Seed")
-//     static let PrivateKey = KnownValue(201, "PrivateKey")
-//     static let PublicKey = KnownValue(202, "PublicKey")
-//     static let MasterKey = KnownValue(203, "MasterKey")
-//     static let asset = KnownValue(300, "asset")
-//     static let Bitcoin = KnownValue(301, "BTC")
-//     static let Ethereum = KnownValue(302, "ETH")
-//     static let network = KnownValue(400, "network")
-//     static let MainNet = KnownValue(401, "MainNet")
-//     static let TestNet = KnownValue(402, "TestNet")
-//     static let BIP32Key = KnownValue(500, "BIP32Key")
-//     static let chainCode = KnownValue(501, "chainCode")
-//     static let DerivationPath = KnownValue(502, "DerivationPath")
-//     static let parentPath = KnownValue(503, "parent")
-//     static let childrenPath = KnownValue(504, "children")
-//     static let parentFingerprint = KnownValue(505, "parentFingerprint")
-//     static let PSBT = KnownValue(506, "PSBT")
-//     static let OutputDescriptor = KnownValue(507, "OutputDescriptor")
-//     static let outputDescriptor = KnownValue(508, "outputDescriptor")
-// }
-// ```
-
 // For definitions see: https://github.com/BlockchainCommons/Research/blob/master/papers/bcr-2023-002-known-value.md#appendix-a-registry
 
 known_value_constant!(IS_A, 1, "isA");
