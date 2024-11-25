60e49e2ce3ec83b6e5c78b8a19d9119eff5ecb92
Wolf McNally <wolf@wolfmcnally.com>
2024-11-25 01:21:24 -0800

    Working on implementing signatures with metadata

--- src/extension/signature/signature_impl.rs
+++ src/extension/signature/signature_impl.rs
@@ -1,7 +1,7 @@
-use anyhow::{bail, Result};
-use bc_components::{DigestProvider, Signature, Signer, SigningOptions, Verifier};
+use anyhow::{ bail, Result };
+use bc_components::{ DigestProvider, Signature, Signer, SigningOptions, Verifier };
 
-use crate::{Envelope, EnvelopeEncodable, EnvelopeError};
+use crate::{ Envelope, EnvelopeEncodable, EnvelopeError };
 #[cfg(feature = "known_value")]
 use crate::extension::known_values;
 
@@ -32,23 +32,37 @@ impl Envelope {
         &self,
         private_key: &dyn Signer,
         options: Option<SigningOptions>,
-        metadata: Option<SignatureMetadata>,
+        metadata: Option<SignatureMetadata>
     ) -> Self {
         let digest = *self.subject().digest().data();
-        let mut signature = Envelope::new(private_key.sign_with_options(&digest as &dyn AsRef<[u8]>, options.clone()).unwrap());
+        let mut signature = Envelope::new(
+            private_key.sign_with_options(&digest as &dyn AsRef<[u8]>, options.clone()).unwrap()
+        );
 
         if let Some(metadata) = metadata {
             if metadata.has_assertions() {
                 let mut signature_with_metadata = signature;
 
-                metadata.assertions().iter().for_each(|assertion| {
-                    signature_with_metadata = signature_with_metadata.add_assertion_envelope(assertion.to_envelope()).unwrap();
-                });
+                metadata
+                    .assertions()
+                    .iter()
+                    .for_each(|assertion| {
+                        signature_with_metadata = signature_with_metadata
+                            .add_assertion_envelope(assertion.to_envelope())
+                            .unwrap();
+                    });
 
                 signature_with_metadata = signature_with_metadata.wrap_envelope();
 
-                let outer_signature = Envelope::new(private_key.sign_with_options(&signature_with_metadata.digest().as_ref(), options).unwrap());
-                signature = signature_with_metadata.add_assertion(known_values::SIGNED, outer_signature);
+                let outer_signature = Envelope::new(
+                    private_key
+                        .sign_with_options(&signature_with_metadata.digest().as_ref(), options)
+                        .unwrap()
+                );
+                signature = signature_with_metadata.add_assertion(
+                    known_values::SIGNED,
+                    outer_signature
+                );
             }
         }
 
@@ -63,9 +77,9 @@ impl Envelope {
     ///
     /// - Returns: The signed envelope.
     pub fn add_signatures(&self, private_keys: &[&dyn Signer]) -> Self {
-        private_keys.iter().fold(self.clone(), |envelope, private_key| {
-            envelope.add_signature(*private_key)
-        })
+        private_keys
+            .iter()
+            .fold(self.clone(), |envelope, private_key| { envelope.add_signature(*private_key) })
     }
 
     #[doc(hidden)]
@@ -75,10 +89,15 @@ impl Envelope {
     ///   - private_keys: An array of signers' `SigningPrivateKey`s and optional `SigningOptions`.
     ///
     /// - Returns: The signed envelope.
-    pub fn add_signatures_opt(&self, private_keys: &[(&dyn Signer, Option<SigningOptions>, Option<SignatureMetadata>)]) -> Self {
-        private_keys.iter().fold(self.clone(), |envelope, (private_key, options, metadata)| {
-            envelope.add_signature_opt(*private_key, options.clone(), metadata.clone())
-        })
+    pub fn add_signatures_opt(
+        &self,
+        private_keys: &[(&dyn Signer, Option<SigningOptions>, Option<SignatureMetadata>)]
+    ) -> Self {
+        private_keys
+            .iter()
+            .fold(self.clone(), |envelope, (private_key, options, metadata)| {
+                envelope.add_signature_opt(*private_key, options.clone(), metadata.clone())
+            })
     }
 
     /// Convenience constructor for a `'signed': Signature` assertion envelope.
@@ -88,11 +107,7 @@ impl Envelope {
     ///   - note: An optional note to be added to the `Signature`.
     ///
     /// - Returns: The new assertion envelope.
-    pub fn make_signed_assertion(
-        &self,
-        signature: &Signature,
-        note: Option<&str>
-    ) -> Self {
+    pub fn make_signed_assertion(&self, signature: &Signature, note: Option<&str>) -> Self {
         let mut envelope = Envelope::new_assertion(known_values::SIGNED, signature.clone());
         if let Some(note) = note {
             envelope = envelope.add_assertion(known_values::NOTE, note);
@@ -118,11 +133,7 @@ impl Envelope {
     ///   - public_key: The potential signer's `Verifier`.
     ///
     /// - Returns: `true` if the signature is valid for this envelope's subject, `false` otherwise.
-    pub fn is_verified_signature(
-        &self,
-        signature: &Signature,
-        public_key: &dyn Verifier,
-    ) -> bool {
+    pub fn is_verified_signature(&self, signature: &Signature, public_key: &dyn Verifier) -> bool {
         self.is_signature_from_key(signature, public_key)
     }
 
@@ -141,7 +152,7 @@ impl Envelope {
     pub fn verify_signature(
         &self,
         signature: &Signature,
-        public_key: &dyn Verifier,
+        public_key: &dyn Verifier
     ) -> Result<Self> {
         self.verify_signature_from_key(signature, public_key)
     }
@@ -155,10 +166,7 @@ impl Envelope {
     ///
     /// - Throws: Throws an exception if any `'signed'` assertion doesn't contain a
     /// valid `Signature` as its object.
-    pub fn has_signature_from(
-        &self,
-        public_key: &dyn Verifier,
-    ) -> Result<bool> {
+    pub fn has_signature_from(&self, public_key: &dyn Verifier) -> Result<bool> {
         self.has_some_signature_from_key(public_key)
     }
 
@@ -173,18 +181,12 @@ impl Envelope {
     ///
     /// - Throws: Throws `EnvelopeError.unverifiedSignature` if the signature is not valid.
     /// valid.
-    pub fn verify_signature_from(
-        &self,
-        public_key: &dyn Verifier,
-    ) -> Result<Self> {
+    pub fn verify_signature_from(&self, public_key: &dyn Verifier) -> Result<Self> {
         self.verify_has_some_signature_from_key(public_key)
     }
 
     /// Checks whether the envelope's subject has a set of signatures.
-    pub fn has_signatures_from(
-        &self,
-        public_keys: &[&dyn Verifier],
-    ) -> Result<bool> {
+    pub fn has_signatures_from(&self, public_keys: &[&dyn Verifier]) -> Result<bool> {
         self.has_signatures_from_threshold(public_keys, None)
     }
 
@@ -204,16 +206,13 @@ impl Envelope {
     pub fn has_signatures_from_threshold(
         &self,
         public_keys: &[&dyn Verifier],
-        threshold: Option<usize>,
+        threshold: Option<usize>
     ) -> Result<bool> {
         self.has_signatures_from_keys_threshold(public_keys, threshold)
     }
 
     /// Checks whether the envelope's subject has a set of signatures.
-    pub fn verify_signatures_from(
-        &self,
-        public_keys: &[&dyn Verifier],
-    ) -> Result<Self> {
+    pub fn verify_signatures_from(&self, public_keys: &[&dyn Verifier]) -> Result<Self> {
         self.verify_signatures_from_threshold(public_keys, None)
     }
 
@@ -234,7 +233,7 @@ impl Envelope {
     pub fn verify_signatures_from_threshold(
         &self,
         public_keys: &[&dyn Verifier],
-        threshold: Option<usize>,
+        threshold: Option<usize>
     ) -> Result<Self> {
         self.verify_signatures_from_keys_threshold(public_keys, threshold)
     }
@@ -242,29 +241,18 @@ impl Envelope {
 
 #[doc(hidden)]
 impl Envelope {
-    fn is_signature_from_key(
-        &self,
-        signature: &Signature,
-        key: &dyn Verifier
-    ) -> bool {
+    fn is_signature_from_key(&self, signature: &Signature, key: &dyn Verifier) -> bool {
         key.verify(signature, self.subject().digest().as_ref())
     }
 
-    fn verify_signature_from_key(
-        &self,
-        signature: &Signature,
-        key: &dyn Verifier,
-    ) -> Result<Self> {
+    fn verify_signature_from_key(&self, signature: &Signature, key: &dyn Verifier) -> Result<Self> {
         if !self.is_signature_from_key(signature, key) {
             bail!(EnvelopeError::UnverifiedSignature);
         }
         Ok(self.clone())
     }
 
-    fn has_some_signature_from_key(
-        &self,
-        key: &dyn Verifier
-    ) -> Result<bool> {
+    fn has_some_signature_from_key(&self, key: &dyn Verifier) -> Result<bool> {
         // Valid signature objects are either:
         //
         // - `Signature` objects, or
@@ -274,8 +262,14 @@ impl Envelope {
         let result = signature_objects.iter().any(|signature_object| {
             let signature_object_subject = signature_object.subject();
             if signature_object_subject.is_wrapped() {
-                if let Ok(outer_signature_object) = signature_object.object_for_predicate(known_values::SIGNED) {
-                    let outer_signature = outer_signature_object.extract_subject::<Signature>().unwrap();
+                if
+                    let Ok(outer_signature_object) = signature_object.object_for_predicate(
+                        known_values::SIGNED
+                    )
+                {
+                    let outer_signature = outer_signature_object
+                        .extract_subject::<Signature>()
+                        .unwrap();
                     if !signature_object_subject.is_signature_from_key(&outer_signature, key) {
                         return false;
                     }
@@ -296,10 +290,7 @@ impl Envelope {
         Ok(result)
     }
 
-    fn verify_has_some_signature_from_key(
-        &self,
-        key: &dyn Verifier
-    ) -> Result<Self> {
+    fn verify_has_some_signature_from_key(&self, key: &dyn Verifier) -> Result<Self> {
         if !self.has_some_signature_from_key(key)? {
             bail!(EnvelopeError::UnverifiedSignature);
         }
@@ -338,14 +329,10 @@ impl Envelope {
 
 impl Envelope {
     pub fn sign(&self, signer: &impl Signer) -> Envelope {
-        self
-            .wrap_envelope()
-            .add_signature(signer)
+        self.wrap_envelope().add_signature(signer)
     }
 
     pub fn verify(&self, verifier: &impl Verifier) -> Result<Envelope> {
-        self
-            .verify_signature_from(verifier)?
-            .unwrap_envelope()
+        self.verify_signature_from(verifier)?.unwrap_envelope()
     }
 }
