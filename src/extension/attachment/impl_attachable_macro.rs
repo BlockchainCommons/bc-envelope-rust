776127eb34fab8a1ce5cbdab2335dfb164e71053
Wolf McNally <wolf@wolfmcnally.com>
2025-04-16 04:37:55 -0700

    Prep for release

--- src/extension/attachment/impl_attachable_macro.rs
+++ src/extension/attachment/impl_attachable_macro.rs
@@ -9,32 +9,6 @@
 /// The target type must have an `attachments: Attachments` field for this macro to work.
 /// It will automatically implement the required `attachments()` and `attachments_mut()`
 /// methods, giving the type access to all the default methods in the `Attachable` trait.
-///
-/// # Examples
-/// ```no_run
-/// # use zewif::{Attachments, impl_attachable, Attachable};
-/// #
-/// struct TransactionRecord {
-///     // Core fields...
-///     tx_id: String,
-///     amount: i64,
-///     // Required field for impl_attachable! to work
-///     attachments: Attachments,
-/// }
-///
-/// // Automatically implement the Attachable trait
-/// impl_attachable!(TransactionRecord);
-///
-/// // Now the type has all Attachable methods available
-/// let mut tx = TransactionRecord {
-///     tx_id: "abcdef".to_string(),
-///     amount: 500000,
-///     attachments: Attachments::new(),
-/// };
-///
-/// // Use methods provided by the Attachable trait
-/// tx.add_attachment("Coffee purchase", "vendor.example", Some("memo.v1"));
-/// ```
 #[macro_export]
 macro_rules! impl_attachable {
     ($type:ty) => {
