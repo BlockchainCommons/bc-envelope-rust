f90a06154e8109c5111b1e9bc7853c51fbcb2953
Wolf McNally <wolf@wolfmcnally.com>
2025-06-21 16:33:32 -0700

    Bump version number

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/bc-envelope/0.31.0")]
+#![doc(html_root_url = "https://docs.rs/bc-envelope/0.32.0")]
 #![warn(rust_2018_idioms)]
 
 //! # Gordian Envelope: A Flexible Container for Structured Data
@@ -20,7 +20,7 @@
 //!
 //! ```toml
 //! [dependencies]
-//! bc-envelope = "0.31.0"
+//! bc-envelope = "0.32.0"
 //! ```
 //!
 //! ## Specification
